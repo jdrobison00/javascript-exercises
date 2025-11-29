089b9859e7b6d9050e9c3af9fd562b64f5c1606c
JD Robison <jdrobison00@gmail.com>
2025-11-29 17:19:07 -0500

    Complete exercise 08, remove from array.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
 const removeFromArray = function() {
+    let args = Array.from(arguments);
+
+    // separate original array and items to remove
+    let original = args[0];
+    let remove = args.slice(1);
+
+    let i = 0;
+    while (i < remove.length) {
+        let removeIndex = original.indexOf(remove[i]);;
+        if (removeIndex >= 0) {
+            original.splice(removeIndex, 1);
+        } else {
+            i++;
+        }
+    }
+    return original;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
