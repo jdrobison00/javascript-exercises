42aa516d9cee90af2623a72bdc082e0130731292
JD Robison <jdrobison00@gmail.com>
2025-11-29 11:40:42 -0500

    Complete Exercise 6, repeat String.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeat) {
+    if (repeat < 0) {
+        return 'ERROR';
+    }
+    output = '';
+    for(let i = 0; i < repeat; i++) {
+        output += str;
+    }
+    return output;
 };
 
 // Do not edit below this line
