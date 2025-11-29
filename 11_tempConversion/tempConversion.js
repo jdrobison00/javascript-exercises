db9152627e501a427615211a4732ef8e38f6303c
JD Robison <jdrobison00@gmail.com>
2025-11-29 18:19:48 -0500

    Complete exercise 11, temp Conversion.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const RATIO_FTOC = 5/9;
+const ZERO_C = 32;
+
+function roundToTenths(num) {
+  return Math.round(num * 10) / 10;
+}
+
+const convertToCelsius = function(fahrenheit) {
+  return roundToTenths((fahrenheit - ZERO_C) * RATIO_FTOC);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return roundToTenths(celsius / RATIO_FTOC +ZERO_C);
 };
 
 // Do not edit below this line
