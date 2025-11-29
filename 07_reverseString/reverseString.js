bfe00567f3ac115e2b66a471bae353f87af09aa7
JD Robison <jdrobison00@gmail.com>
2025-11-29 11:48:55 -0500

    Complete exercise 07, reverse String.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    strArray = str.split('');
+    
+    let reversed = [];
+    while(strArray.length > 0) {
+        reversed.push(strArray.pop());
+    }
 
+    return reversed.join('');
 };
 
 // Do not edit below this line
