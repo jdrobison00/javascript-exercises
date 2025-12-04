e82bc4818b2ab92f33fbba716d2b7cf90318d6b5
JD Robison <jdrobison00@gmail.com>
2025-12-03 23:19:28 -0500

    Complete exercise 12 in JavaScript Exercises.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((original, current) => original + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((original, current) => original * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let product = 1;
+  for(i = a; i >= 1; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
