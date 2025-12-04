e142c1dd2c2d956fbad0d552cb4cade9e6584c94
JD Robison <jdrobison00@gmail.com>
2025-12-04 00:20:25 -0500

    Finish exercise 16, find the oldest.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    function getAge(person) {
+        if (!("yearOfDeath" in person)) {
+            return 2025 - person.yearOfBirth;
+        }
+        return person.yearOfDeath - person.yearOfBirth;
+    }
 
+    console.table(people);
+    people.sort( (a, b) =>
+        getAge(a) > getAge(b) ? -1 : 1
+    );
+    console.table(people);
+    return people[0];
+    
 };
 
 // Do not edit below this line
