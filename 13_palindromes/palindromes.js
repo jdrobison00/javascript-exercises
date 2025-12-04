52bfdce8254014b439ece6045587fbfa824601ce
JD Robison <jdrobison00@gmail.com>
2025-12-03 23:45:12 -0500

    Complete exercise 13, palindromes.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphaNumerical = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    const cleanedArr = string.toLowerCase().split("")
+    .filter(char => alphaNumerical.includes(char));
 
+    let cleanedString = cleanedArr.slice().join("");
+
+    let reversedArr = [];
+    for(let i = 0; i < cleanedString.length; i++) {
+        reversedArr.push(cleanedArr.pop());
+    }
+
+    console.log(reversedArr);
+
+    reversedString = reversedArr.join("");
+
+    return reversedString === cleanedString;
 };
 
 // Do not edit below this line
